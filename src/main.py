d449d7facdd1c5344578206d0e24d9eb82ebb505
RaZ3i <tony.ni2016@yandex.ru>
2024-12-09 21:35:09 +0400

    Blog ver. 1.0

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 
 from src.auth.router import router as auth_router
@@ -11,22 +10,3 @@ app.mount("/static", StaticFiles(directory="src/static", html=True), name="stati
 app.include_router(auth_router)
 app.include_router(profile_oper_router)
 app.include_router(pages_router)
-
-# origins = [
-#     "http://127.0.0.1:5500",
-#     "http://localhost:5500",
-#     "http://127.0.0.1:8087",
-#     "http://localhost:8087",
-# ]
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=origins,
-#     allow_credentials=True,
-#     allow_methods=["GET", "POST", "DELETE", "PATCH", "PUT", "OPTIONS"],
-#     allow_headers=[
-#         "Content-Type",
-#         "Access-Control-Allow-Headers",
-#         "Access-Control-Allow-Origin",
-#         "Authorization",
-#     ],
-# )
